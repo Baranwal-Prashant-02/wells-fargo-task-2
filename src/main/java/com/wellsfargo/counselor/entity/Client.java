812b120f03c3e8cd2e929f19cdae063517c296f3
Prashant Kr. Baranwal <baranwalprashant2@gmail.com>
2026-06-09 17:34:50 +0530

    Update Client.java

--- src/main/java/com/wellsfargo/counselor/entity/Client.java
+++ src/main/java/com/wellsfargo/counselor/entity/Client.java
@@ -9,22 +9,48 @@ public class Client {
     @GeneratedValue()
     private long clientId;
 
+    @ManyToOne
+    private Advisor advisor;
+
     @Column(nullable = false)
     private String firstName;
 
     @Column(nullable = false)
     private String lastName;
 
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String email;
+
     protected Client() {
+
     }
 
-    public Client(String firstName, String lastName) {
+    public Client(Advisor advisor, String firstName, String lastName,
+                  String address, String phone, String email) {
+        this.advisor = advisor;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
     }
 
-    public long getClientId() {
+    public Long getClientId() {
         return clientId;
+    }
+
+    public Advisor getAdvisor() {
+        return advisor;
+    }
+
+    public void setAdvisor(Advisor advisor) {
+        this.advisor = advisor;
     }
 
     public String getFirstName() {
@@ -41,5 +67,29 @@ public class Client {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
